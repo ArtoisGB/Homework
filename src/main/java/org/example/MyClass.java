c36b880960e81fc6ef66f90c6e040f04d11cce8f
ArturGB <artois.gb@gmail.com>
2023-10-25 17:23:38 +0200

    formating

--- src/main/java/org/example/MyClass.java
+++ src/main/java/org/example/MyClass.java
@@ -6,10 +6,10 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("aloha");
         System.out.println("first task");
-        int [] firstTable = {1,3,2,7,6,0};
-        int [] secondTable = {1,3,2,7,6,0};
+        int[] firstTable = {1, 3, 2, 7, 6, 0};
+        int[] secondTable = {1, 3, 2, 7, 6, 0};
 
-        FirstHomework firstHomework= new FirstHomework();
+        FirstHomework firstHomework = new FirstHomework();
         firstHomework.bubbleSort(firstTable);
         System.out.println(Arrays.toString(firstTable));
         System.out.println(Arrays.toString(firstHomework.bubbleSort(secondTable)));

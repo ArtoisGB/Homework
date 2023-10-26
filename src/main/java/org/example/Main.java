9c503003598427f9665bc03940912a638ef2ad78
ArturGB <artois.gb@gmail.com>
2023-10-26 20:14:03 +0200

    secure sizetable in front of Strings or negative int

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         final String SIZE_TABLE = "Put a desire size table with number greater than 1";
         System.out.println("first task");
-
+        SizeOfTable sizeOfTable = new SizeOfTable();
         RandomTable randomTable = new RandomTable();
         FirstHomework firstHomework = new FirstHomework();
-        Scanner sc = new Scanner(System.in);
         System.out.println(SIZE_TABLE);
-        int sizeTable = sc.nextInt();
-        int[] newTable = randomTable.createTable(sizeTable);
+        int lengthTable = sizeOfTable.sizeTable();
+        int[] newTable = randomTable.createTable(lengthTable);
+        System.out.println("Table before sorting:");
         System.out.println(Arrays.toString(newTable));
         System.out.println(Arrays.toString(firstHomework.bubbleSort(newTable)));
 

53727b4aae8468c64f8aadc3d1f1beff9c8f9eda
ArturGB <artois.gb@gmail.com>
2023-11-14 15:56:28 +0100

    add logger

--- src/main/java/org/airport/Base.java
+++ src/main/java/org/airport/Base.java
@@ -1,10 +1,14 @@
 package org.airport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Random;
 
 public class Base {
     public Connections connections = new Connections();
     public TypeOfFlight typeOfFlight = new TypeOfFlight();
     public CountryReader countryReader = new CountryReader();
+    protected static final Logger LOGGER = LogManager.getLogger(Base.class);
     public Random random = new Random();
 }
